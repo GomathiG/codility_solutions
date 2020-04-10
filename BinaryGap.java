ef5e14874b60713fc32d2ec9e3b302f0617a5511
GomathiG <32731415+GomathiG@users.noreply.github.com>
2020-04-10 15:07:51 +0530

    Update BinaryGap.java

--- BinaryGap.java
+++ BinaryGap.java
@@ -12,28 +12,25 @@ public class BinaryGap {
 		int l = Integer.parseInt(br.readLine());
 		String binary = Integer.toBinaryString(l);
 		String[] arr = binary.split("");
-		int count = 0,  zeroCount=0;
+		int count = 0, zeroCount = 0;
 		boolean isSet = false, isGap = false;
-		for(int i=0;i<arr.length;i++) {
-			
+		for (int i = 0; i < arr.length; i++) {
 			System.out.println(binary.charAt(i));
-			if(arr[i].equals("1")) {
+			if (arr[i].equals("1")) {
 				isSet = true;
 			}
-			if(arr[i].equals("0") && isSet){
+			if (arr[i].equals("0") && isSet) {
 				zeroCount++;
 			}
-			if(arr[i].equals("1") && isSet && zeroCount>0){
+			if (arr[i].equals("1") && isSet && zeroCount > 0) {
 				isGap = true;
 			}
-			if(isGap && count<zeroCount) {
+			if (isGap && count < zeroCount) {
 				isGap = false;
 				count = zeroCount;
 				zeroCount = 0;
 			}
 		}
-		
 		System.out.println(count);
 	}
-
 }
